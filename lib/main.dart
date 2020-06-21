fdbb9cfb11758f546deb92a37b33c9043d880881
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-06-21 20:54:40 +0530

    Add questions

--- lib/main.dart
+++ lib/main.dart
@@ -27,27 +27,15 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoreKeeper=[
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ) ,
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
   ];
+
+  List<String> questions=[
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int questionNo=0;
 
   @override
   Widget build(BuildContext context) {
@@ -60,8 +48,9 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
-              child: Text(
-                'This is where the question text will go.',
+              child:
+              Text(
+                questions[questionNo],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -86,6 +75,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  questionNo++;
+                });
+
               },
             ),
           ),
@@ -104,6 +97,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  questionNo++;
+                });
+
               },
             ),
           ),
